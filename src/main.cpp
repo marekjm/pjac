17c02ac792d675b97340eecf50a475eade5c289c
Marek Marecki <marekjm@ozro.pw>
2016-01-10 19:53:17 +0100

    Foundation code

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,39 @@
 using namespace std;
 
 
+class Exception {
+    public:
+        virtual string what() const = 0;
+        Exception() {}
+        virtual ~Exception() {}
+};
+
+
+class IOException: public Exception {
+};
+
+class ReadException: public IOException {
+    string filename;
+
+    public:
+        string what() const override {
+            return filename;
+        }
+        ReadException(): filename("") {}
+        ReadException(const string& f): filename(f) {}
+};
+
+class NoSuchFile: public ReadException {
+    string filename;
+
+    public:
+        string what() const override {
+            return filename;
+        }
+        NoSuchFile(const string& f): filename(f) {}
+};
+
+
 namespace support {
     namespace env {
         bool isfile(const string& path) {
@@ -21,13 +54,38 @@ namespace support {
             return true;
         }
     }
+
+    namespace io {
+        vector<string> readlines(const string& filename) {
+            vector<string> lines;
+            string line;
+
+            if (!support::env::isfile(filename)) {
+                throw NoSuchFile(filename);
+            }
+
+            ifstream in(filename, ios::in | ios::binary);
+            if (!in) {
+                throw ReadException(filename);
+            }
+
+            while (getline(in, line)) {
+                lines.push_back(line);
+            }
+
+            return lines;
+        }
+    }
 }
 
 
 int main(int argc, char **argv) {
     // setup command line arguments vector
     vector<string> args;
-    string option;
+
+    for (int i = 1; i < argc; ++i) {
+        args.push_back(string(argv[i]));
+    }
 
     string filename(""), compilename("");
 
@@ -36,34 +94,20 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    ////////////////////////////////
-    // FIND FILENAME AND COMPILENAME
+    if (compilename == "") {
+        compilename = "out.asm";
+    }
+
     filename = args[0];
     if (!filename.size()) {
         cout << "fatal: no file to assemble" << endl;
         return 1;
     }
     if (!support::env::isfile(filename)) {
-        cout << "fatal: could not open file: " << filename << endl;
+        cout << "fatal: no such file: " << filename << endl;
         return 1;
     }
-
-    if (compilename == "") {
-        compilename = "out.asm";
-    }
-
-
-    ////////////////
-    // READ LINES IN
-    ifstream in(filename, ios::in | ios::binary);
-    if (!in) {
-        cout << "fatal: file could not be opened: " << filename << endl;
-        return 1;
-    }
-
-    vector<string> lines;
-    string line;
-    while (getline(in, line)) { lines.push_back(line); }
+    vector<string> lines = support::io::readlines(filename);
 
     return 0;
 }
