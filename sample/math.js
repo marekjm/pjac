64d418853dd2443da00a8df353452397f1e092a1
Marek Marecki <marekjm@ozro.pw>
2016-01-11 23:32:03 +0100

    Update math sample

--- sample/math.js
+++ sample/math.js
@@ -1,19 +1,19 @@
-function add
+function add -> int {
     var int a;
     asm iadd 0 (arg 1 0) (arg 2 1);
     return a;
-end
+}
 
-function print
+function print -> void {
     asm print (arg 1 0);
     return;
-end
+}
 
-function main
+function main -> int {
     var int a = 20;
     var int b = 22;
     var int c;
     c = add(a, b);
     print(c);
     return 0;
-end
+}
