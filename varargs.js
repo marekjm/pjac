3a64974a87621a1691df7dd7d7ea2447a5bc9781
Marek Marecki <marekjm@ozro.pw>
2016-02-01 14:29:35 +0100

    Temporarily abort variable length parameter lists

--- varargs.js
+++ varargs.js
@@ -1,8 +1,5 @@
-function echo_one(auto msg) { asm echo msg; }
-function echo(auto msg...) { asm echo msg; }
-
-function print_one(auto msg) { asm print msg; }
-function print(auto msg...) { asm print msg; }
+function echo(auto msg) { asm echo msg; }
+function print(auto msg) { asm print msg; }
 
 function main() -> int {
     print("Hello World!");
